82d0e7a3c5970fdde4ee8f7bc35a3a37f1f327e4
kreza <kashif.reza@icreon.com>
2019-08-04 13:54:08 +0530

    Added TemplatePatternDemo project

---
+++ Design Patterns/TemplatePatternDemo/TemplatePatternDemo/Bread.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TemplatePatternDemo
+{
+    /// <summary>
+    /// The AbstractClass participant which contains the template method.
+    /// </summary>
+    abstract class Bread
+    {
+        public abstract void MixIngredients();
+
+        public abstract void Bake();
+
+        public virtual void Slice()
+        {
+            Console.WriteLine("Slicing the " + GetType().Name + " bread!");
+        }
+
+        /// <summary>
+        /// This is the Template Method
+        /// Template Method pattern defines in what order certain steps should occur,
+        /// but can optionally leave the specific details of those steps to be implemented by other classes.
+        ///
+        /// So here making steps/ordering is defined that first it will --
+        /// Mixing Ingredients -> then Baking will happen -> then Slicing of bread
+        /// </summary>
+        public void Make()
+        {
+            MixIngredients();
+            Bake();
+            Slice();
+        }
+    }
+}
---
+++ Design Patterns/TemplatePatternDemo/TemplatePatternDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePatternDemo
+{
+    /// <summary>
+    /// https://exceptionnotfound.net/the-daily-design-pattern-template-method/
+    /// Template Method pattern defines in what order certain steps should occur, but can optionally leave the specific details of those steps to be implemented by other classes
+    ///
+    /// Type: Behavioral
+    /// 
+    /// Good For: Creating an outline of an algorithm but letting specific steps be implemented by other classes.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            Console.WriteLine("\n--------------------------------\n");
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            Console.WriteLine("\n--------------------------------\n");
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+
+            Console.ReadKey();
+        }
+    }
+}
